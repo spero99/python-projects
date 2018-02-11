fcd118c19ddec1d94cb6eb39d3979252901b9e28
spero99 <sperako948@gmail.com>
2018-02-11 18:11:58 +0200

    changed first day of the week to sundqay

--- calendar project/mycalendar.py
+++ calendar project/mycalendar.py
@@ -1,7 +1,7 @@
 #p17095/creation of a table like calendar of the selected month
 
 import calendar
-c=calendar.TextCalendar(calendar.MONDAY)
+c=calendar.TextCalendar(calendar.SUNDAY)
 year = int(input("enter the year you want:"))
 month = int(input("enter the month you want:"))
 str=c.formatmonth(year,month)
