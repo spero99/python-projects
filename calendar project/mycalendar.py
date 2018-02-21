57bfbffc5e1b67dd73d20217735380fd18ebf2cd
spero99 <sperako948@gmail.com>
2018-02-21 19:28:48 +0200

    calendar commit,no format on weeks

--- calendar project/mycalendar.py
+++ calendar project/mycalendar.py
@@ -1,21 +1,28 @@
 #p17095/creation of a table like calendar of the selected month
-
-
 import datetime
 
 year = int(input("enter the year you want:"))
 month = int(input("enter the month you want:"))
 FirstOfMonth=datetime.date(year , month,1)
-print(FirstOfMonth.strftime("%A"))                                                                           #test line
-days="S M T W T F S"
-print(+month+year)
-print(days)
+start=FirstOfMonth.strftime("%w")
+days="S\tM\tT\tW\tT\tF\tS"
+#print(+month+year)                                                              #testline
+#print(days)                                                                     #testline
+#print(start)                                                                    #testline
+month_data=((31,"January"),(28,"February"),(31,"March"),(30,"April"),(31,"May"),(30,"June"),(31,"July"),(31,"August"),(30,"September"),( 31,"Ocotber"),(30,"November"),(31,"December"))
+daysnum=((1,"Sunday"),(2,"Monday"),(3,"Tuesday"),(4,"Wednesday"),(5,"Thursday"),(6,"Friday"),(7,"Saturday"))
 
-month_data=[(1,31,"January"),(2,28,"February"),(3,31,"March"),(4,30,"April"),(5,31,"May"),(6,30,"June"),(7,31,"July"),(8,31,"August"),(9,30,"September"),(10, 31,"Ocotber"),(11,30,"November"),(12,31,"December")]
-daysnum=((1,Sunday),(2,Monday),(3,Tuesday),(4,Wednesday),(5,Thursday),(6,Friday),(7,Saturday))
-print(month_data)
 
-if year%4==0 or (year%100==0 and year%400 !=0)
-    month_data[1][1]=29
+if year%4==0 or (year%100==0 and year%400 !=0):
+    month_data[1][0]=29
 
-print(month_data)
+#print(month_data)                                                               #testline
+max_days=month_data[month-1][0]
+calendar=[]
+
+
+for i in range(1,max_days+1):
+    calendar.append(i)
+    i=i+1
+
+print(calendar)
